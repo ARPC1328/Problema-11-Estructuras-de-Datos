59acb211593addec07da14b5e072dc0713069337
Brayan-exe <78459098+Brayan-exe@users.noreply.github.com>
2021-02-02 21:55:59 -0600

    Update DePasajeros.java

--- DePasajeros.java
+++ DePasajeros.java
@@ -1,5 +1,6 @@
+package Ejercicio11;
 
-public class DePasajeros {
+public abstract class DePasajeros extends Camion {
     
     private int totPasajeros;
 
@@ -7,11 +8,24 @@ public class DePasajeros {
     }
 
     public DePasajeros(String marca, String numMotor, String placas, double costoCamion, int totPasajeros) {
+        super (marca, numMotor, placas, costoCamion);
         this.totPasajeros = totPasajeros;
     }
 
+    public int getTotPasajeros() {
+        return totPasajeros;
+    }
+
+    public void setTotPasajeros(int totPasajeros) {
+        this.totPasajeros = totPasajeros;
+    }
+    
     @Override
     public String toString() {
-        return "DePasajeros{" + "totPasajeros=" + totPasajeros + '}'; //super.toString();
-     } 
+        return "DePasajeros{" + "totPasajeros=" + totPasajeros + super.toString();
+    }
+   
+   public double calculaCostoServicio(double cantKilometros){
+       return (0.01 * super.getCostoCamion() / totPasajeros) * cantKilometros;
+   }
 }
